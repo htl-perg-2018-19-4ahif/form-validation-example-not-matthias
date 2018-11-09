273bdf6c97cd90baaebc238ac48c09853e912ff9
Matthias <matthias.heiden.mh@gmail.com>
2018-11-09 16:08:41 +0100

    added some tslint stuff and first version of form validation

---
+++ form-validation.ts
@@ -0,0 +1,65 @@
+/**
+ * Set the disabled attribute for an element
+ * @param element the specified element
+ * @param disabled boolean whether the element should be disabled
+ */
+const setDisabled = (element: string, disabled: boolean) => {
+    $(element).attr("disabled", disabled.toString());
+};
+
+/**
+ * Shows the element when the text is empty
+ * @param element the html element (id or class)
+ * @param text the string which will be checked
+ */
+const showIfEmpty = (element: string, text: string) => {
+    if (text)
+        $(element).hide();
+    else
+        $(element).show();
+};
+
+
+let firstname: string = "";
+let lastname: string = "";
+let email: string = "";
+let mediaChannel: string = "";
+let otherMedia: string = "";
+
+$("#firstName").keydown(() => {
+    console.log(`Keydown: ${String($("#firstName").val())}`);
+});
+
+$("#firstName").keyup(() => {
+    console.log(`Keyup: ${String($("#firstName").val())}`);
+});
+
+$("#firstName").keypress(() => {
+    console.log(`Keypress: ${String($("#firstName").val())}`);
+});
+
+
+$("#firstName").keydown(() => {
+    firstname = String($("#firstName").val());
+    showIfEmpty("#firstNameMandatory", firstname);
+});
+
+$("#lastName").keydown(() => {
+    console.log(String($("#lastName").val()));
+    lastname = String($("#lastName").val());
+    showIfEmpty("#lastNameMandatory", lastname);
+});
+
+$("#email").keydown(() => {
+    email = String($("#email").val());
+    showIfEmpty("#emailMandatory", email);
+});
+
+$("#mediaChannelSelect").change(() => {
+    console.log("mediaChannelSelect");
+});
+
+$("#otherMediaChannel").change(() => {
+    console.log("otherMediaChannel");
+});
+
