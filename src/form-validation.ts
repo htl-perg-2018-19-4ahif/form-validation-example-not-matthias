79fccae2926f1d4565cb128f2db086afb438862b
Matthias <matthias.heiden.mh@gmail.com>
2018-11-09 17:53:35 +0100

    created event handler functions and implemented submit logic

--- src/form-validation.ts
+++ src/form-validation.ts
@@ -1,3 +1,21 @@
+
+//
+// Variables
+//
+
+// Contains booleans, whether the elements are valid or not
+let canSubmit: any = {
+    validEmail: false,
+    validFirstname: false,
+    validLastname: false
+};
+
+
+
+//
+// Helper functions
+//
+
 /**
  * Set the disabled attribute for an element
  * @param element the specified element
@@ -8,8 +26,8 @@ const setDisabled = (element: string, disabled: boolean) => {
 };
 
 /**
- * Shows the element when the text is empty
- * @param element the html element (id or class)
+ * Shows the element when the specified string is empty
+ * @param element the html element id or class
  * @param text the string which will be checked
  */
 const showIfEmpty = (element: string, text: string) => {
@@ -20,44 +38,68 @@ const showIfEmpty = (element: string, text: string) => {
 };
 
 
-let firstname: string = '';
-let lastname: string = '';
-let email: string = '';
-let mediaChannel: string = '';
-let otherMedia: string = '';
 
-$('#firstName').keyup(() => {
-    firstname = String($('#firstName').val());
+//
+// Event handlers
+//
+const checkFirstname = () => {
+    const firstname = String($('#firstName').val());
+
+    // Show a warning if firstname is empty
     showIfEmpty('#firstNameMandatory', firstname);
-});
 
-$('#lastName').keyup(() => {
-    lastname = String($('#lastName').val());
-    showIfEmpty('#lastNameMandatory', lastname);
-});
+    canSubmit.validFirstname = (firstname) ? true : false;
+};
 
-$('#email').keyup(() => {
-    email = String($('#email').val());
+const checkLastname = () => {
+    const email = String($('#lastName').val());
 
-    // If someone wants the newsletter, they must specify an email
-    if ($('#newsletter').prop('checked'))
-        showIfEmpty('#emailMandatory', email);
-});
+    // Show a warning if lastname is empty
+    showIfEmpty('#lastNameMandatory', email);
 
-$('#newsletter').change(() => {
-    // If someone wants the newsletter, they must specify an email
+    canSubmit.validLastname = (email) ? true : false;
+};
+
+const checkEmail = () => {
     if ($('#newsletter').is(':checked')) {
+        // Show a warning if email is empty
         showIfEmpty('#emailMandatory', String($('#email').val()));
+
+        canSubmit.validEmail = true;
     } else {
+        // Hide the warning if the newsletter checkbox is not enabled
         $('#emailMandatory').hide();
+
+        canSubmit.validEmail = true;
     }
-});
+};
 
-
-$('#mediaChannelSelect').change(() => {
+const checkMediaChannel = () => {
     // Only show 'otherMediaChannel' if 'Other' selected
     if ($('#mediaChannelSelect').val() === 'Other')
         $('#otherMediaChannel').show();
     else
         $('#otherMediaChannel').hide();
-});
+};
+
+const checkSubmit = () => {
+    if (canSubmit.validEmail && canSubmit.validFirstname && canSubmit.validLastname)
+        setDisabled('#submit', false);
+    else
+        setDisabled('#submit', true);
+};
+
+
+
+//
+// Events
+//
+$('#firstName').keyup(() => checkFirstname());
+
+$('#lastName').keyup(() => checkLastname());
+
+$('#email').keyup(() => checkEmail());
+
+$('#newsletter').change(() => checkEmail());
+
+$('#mediaChannelSelect').change(() => checkMediaChannel());
